52c0a4552ecb69f8ecd987a796e710000f954061
Lincoln Gibson <lincolngibson7@gmail.com>
2023-06-08 17:19:05 -0300

    Add routes to the App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Books from './pages/Books';
+import Categories from './pages/Categories';
+import Layouts from './components/Layout';
 import './App.css';
 
 function App() {
-  return <div className="App">Learn React</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Layouts />}>
+        <Route path="/books" element={<Books />} />
+        <Route path="/categories" element={<Categories />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
