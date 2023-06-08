a072dfeb23bb26d7ba742bae9bf0ff288e3a51a5
Lincoln Gibson <lincolngibson7@gmail.com>
2023-06-08 20:37:12 -0300

    Add dummy  text to render on the page.

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
 const Categories = () => (
-  <div>Categories</div>
+  <div>
+    <h2>Categories</h2>
+    <p>
+      Lorem ipsum dolor sit amet,
+      consectetur adipisicing elit.
+      Ratione totam debitis rerum
+      deleniti temporibus molestiae
+      asperiores mollitia eligendi. Praesentium, minus porro autem
+      magnam nostrum consequuntur ad rerum veniam adipisci aliquid!
+    </p>
+  </div>
 );
 
 export default Categories;
