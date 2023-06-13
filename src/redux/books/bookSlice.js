8172c2136e856d0425dcf836e55377fdbda47997
Lincoln Gibson <lincolngibson7@gmail.com>
2023-06-13 13:25:33 -0300

    Import createAsyncThunk, .env and create api call.

--- src/redux/books/bookSlice.js
+++ src/redux/books/bookSlice.js
@@ -1,5 +1,7 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
+
+const apiKey = process.env.REACT_APP_API_KEY;
 
 const initialState = {
   books: [
@@ -23,6 +25,17 @@ const initialState = {
     },
   ],
 };
+
+export const fetchBooks = createAsyncThunk('books/fetchBooks', async () => {
+  try {
+    const response = await axios.get(
+      `https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/${apiKey}/books`,
+    );
+    return response.data;
+  } catch (error) {
+    return error.message;
+  }
+});
 
 const bookSlice = createSlice({
   name: 'books',
