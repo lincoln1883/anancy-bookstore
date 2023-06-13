5f050e4b974643be047a124394d6d5b4d4c69827
Lincoln Gibson <lincolngibson7@gmail.com>
2023-06-13 17:59:33 -0300

    Import functions from book slice and render logic.

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,20 +1,51 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  getAllBooks, getBooksError, getBooksStatus, fetchBooks,
+} from '../redux/books/bookSlice';
 import Book from './Book';
 import BookInput from './BookInput';
 
 const BookList = () => {
-  const book = useSelector((state) => state.books);
+  const dispatch = useDispatch();
+
+  const book = useSelector(getAllBooks);
+  const booksStatus = useSelector(getBooksStatus);
+  const booksError = useSelector(getBooksError);
+
+  useEffect(() => {
+    if (booksStatus === 'idle') {
+      dispatch(fetchBooks());
+    }
+  }, [booksStatus, dispatch]);
+
+  let display;
+
+  if (booksStatus === 'loading') {
+    display = <div>Loading...</div>;
+  } else if (booksStatus === 'success') {
+    const keys = Object.keys(book);
+    if (keys.length === 0) {
+      display = <div>No books available</div>;
+    }
+    display = keys.map((key) => (
+      book[key].map((bookItem) => (
+        <Book
+          key={bookItem.item_id}
+          book={bookItem}
+        />
+      ))
+    ));
+  } else if (booksStatus === 'failed') {
+    display = <p>{booksError}</p>;
+  }
 
   return (
     <>
       <h2>BookList</h2>
-      {book.books.map((book) => (
-        <Book
-          key={book.id}
-          book={book}
-        />
-      ))}
+      <div>
+        {display}
+      </div>
       <BookInput />
     </>
   );
