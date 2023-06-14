a403b1b5e2f5be02dddeffb0da39a5f5bafe9b6f
Lincoln Gibson <lincolngibson7@gmail.com>
2023-06-14 19:26:03 -0300

    Edit styling of button.

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Button = ({ onClick, children }) => (
-  <button type="button" onClick={onClick}>{children}</button>
+  <button type="button" className="bg-blue-700 text-white text-sm rounded-md h-8 w-24 " onClick={onClick}>{children}</button>
 );
 Button.propTypes = {
   onClick: PropTypes.func.isRequired,
