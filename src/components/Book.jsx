6fa544b6320848ee97db24284bee3aa7c48c7bb4
Lincoln Gibson <lincolngibson7@gmail.com>
2023-06-14 10:21:33 -0300

    Refactor prop types and destructor incoming props.

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -4,21 +4,19 @@ import { useDispatch } from 'react-redux';
 import Button from './Button';
 import { removeBook } from '../redux/books/bookSlice';
 
-const Book = ({ book }) => {
-  console.log(book);
+const Book = ({ id, title, author }) => {
   const dispatch = useDispatch();
 
   const handleRemoveBook = () => {
-    const id = book.item_id;
     dispatch(removeBook(id));
   };
 
   return (
     <>
       <div>
-        <span>{book.title}</span>
+        <span>{title}</span>
         {' - '}
-        <span>{book.author}</span>
+        <span>{author}</span>
         {' - '}
         <Button type="button" onClick={handleRemoveBook}>Remove</Button>
       </div>
@@ -27,11 +25,9 @@ const Book = ({ book }) => {
 };
 
 Book.propTypes = {
-  book: PropTypes.shape({
-    item_id: PropTypes.number.isRequired,
-    title: PropTypes.string.isRequired,
-    author: PropTypes.string.isRequired,
-  }).isRequired,
+  id: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
 };
 
 export default Book;
