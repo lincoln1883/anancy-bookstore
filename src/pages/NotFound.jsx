49cdeb0ef6e19e849130d4e45888fc3ea6114648
Lincoln Gibson <lincolngibson7@gmail.com>
2023-06-15 09:48:03 -0300

    Created a not found page.

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => (
+  <div>
+    <p className="text-center font-bold text-6xl">
+      404 Not Found
+    </p>
+  </div>
+);
+
+export default NotFound;
