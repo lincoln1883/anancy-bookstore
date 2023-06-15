5f1bdfd55b8b55cac451228d4632b02d53fcb697
Lincoln Gibson <lincolngibson7@gmail.com>
2023-06-15 09:48:34 -0300

    Import the not found component.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Books from './pages/Books';
 import Categories from './pages/Categories';
 import Layouts from './components/Layout';
+import NotFound from './pages/NotFound';
 import './App.css';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Layouts />}>
           <Route path="/books" element={<Books />} />
           <Route path="/categories" element={<Categories />} />
+          <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
     </Router>
